292287f3d06ca3f657fd1c6ba4e5a82fcfc59117
well0924 <well414965@gmail.com>
2023-10-08 21:13:51 +0900

    refactor 로그인 관련 컨트롤러

--- src/main/java/co/kr/board/controller/view/LoginController.java
+++ src/main/java/co/kr/board/controller/view/LoginController.java
@@ -1,28 +1,21 @@
 package co.kr.board.controller.view;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import co.kr.board.domain.Member;
-import co.kr.board.domain.Dto.MemberDto;
 import co.kr.board.service.MemberService;
 import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/page/login/*")
+@RequestMapping("/page/login")
 public class LoginController {
 	
 	private final MemberService service;
-	
+
+	//로그인 페이지
 	@GetMapping("/loginpage")
 	public ModelAndView loginPage(
 			@RequestParam(value="error",required = false) String error, 
@@ -37,66 +30,8 @@ public class LoginController {
 
 		return mv;
 	}
-	
-	@GetMapping("/memberjoin")
-	public ModelAndView mebmerJoin(){
-		
-		ModelAndView mv = new ModelAndView();
-		
-		mv.setViewName("login/memberjoin");
 
-		return mv;
-	}
-	
-	@GetMapping("/memberdelete")
-	public ModelAndView memberDelete(){
-		
-		ModelAndView mv = new ModelAndView();
-		
-		mv.setViewName("login/memberdelete");
-
-		return mv;
-	}
-	
-	@GetMapping("/memberupdate/{id}")
-	public ModelAndView memberUpdate(@PathVariable("id") Integer userIdx, MemberDto.MemeberResponseDto dto){
-		
-		ModelAndView mv = new ModelAndView();
-		
-		dto = service.getMember(userIdx);
-		
-		mv.addObject("detail", dto);
-		mv.setViewName("login/membermodify");
-		
-		return mv;
-	}
-	
-	@GetMapping("/adminlist")
-	public ModelAndView adminList(@PageableDefault(sort="id",direction = Sort.Direction.DESC,size=5)Pageable pageable){
-		
-		ModelAndView mv = new ModelAndView();
-		
-		Page<MemberDto.MemeberResponseDto>list= service.findAll(pageable);
-				
-		mv.addObject("memberlist", list);
-		mv.setViewName("admin/index");
-		
-		return mv;
-	}
-	
-	@GetMapping("/detail/{id}")
-	public ModelAndView memberDetail(@PathVariable(value="id")Integer userIdx, MemberDto.MemeberResponseDto dto){
-		
-		ModelAndView mv = new ModelAndView();
-		
-		dto = service.getMember(userIdx);
-		
-		mv.addObject("detail", dto);
-		mv.setViewName("login/membermodify");
-		
-		return mv;
-	}
-	
+	//회원 아이디 찾기
 	@GetMapping("/finduserid")
 	public ModelAndView findUserIdPage(){
 		ModelAndView mv = new ModelAndView();
@@ -105,7 +40,8 @@ public class LoginController {
 		
 		return mv;
 	}
-	
+
+	//회원 비밀번호 재설정
 	@GetMapping("/finduserpw")
 	public ModelAndView findUserPw(){
 		ModelAndView mv = new ModelAndView();
