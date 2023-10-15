2446f523321e08d3d35eb36ad1d70021a2c85d10
well0924 <well414965@gmail.com>
2023-10-15 18:36:52 +0900

    feat 회원 검색기능 및 어드민 페이지 추가

---
+++ src/main/java/co/kr/board/domain/SearchType.java
@@ -0,0 +1,30 @@
+package co.kr.board.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum SearchType {
+    //제목
+    TITLE,
+    //회원 아이디
+    ID,
+    //작성자
+    AUTHOR,
+    CONTENTS,
+    //회원 이름
+    MEMBERNAME,
+    EMAIL,
+    ALL;
+
+    public static SearchType toSearch(String searchType){
+        return switch (searchType){
+            case "boardTitle"->TITLE;
+            case "id"->ID;
+            case "memberName"->MEMBERNAME;
+            case "boardAuthor"->AUTHOR;
+            case "memberEmail"->EMAIL;
+            case "boardContents"->CONTENTS;
+            default -> ALL;
+        };
+    }
+}
