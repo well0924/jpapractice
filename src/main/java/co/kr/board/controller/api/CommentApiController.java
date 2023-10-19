f5184e8e0549dadbdf49080ee948a875acdcc09a
well0924 <well414965@gmail.com>
2023-10-19 21:44:58 +0900

    doc 주석추가

--- src/main/java/co/kr/board/controller/api/CommentApiController.java
+++ src/main/java/co/kr/board/controller/api/CommentApiController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +26,7 @@ import co.kr.board.config.Exception.dto.Response;
 import co.kr.board.domain.Dto.CommentDto;
 import co.kr.board.service.CommentService;
 import lombok.AllArgsConstructor;
+
 @Log4j2
 @RestController
 @AllArgsConstructor
@@ -35,12 +35,15 @@ public class CommentApiController {
 	
 	private final CommentService service;
 	private final MemberRepository memberRepository;
+
+	//댓글목록
 	@GetMapping("/list/{id}")
 	public Response<List<CommentDto.CommentResponseDto>>getBoardComments(@PathVariable(value="id")Integer boardId)throws Exception{
 		List<CommentDto.CommentResponseDto>list = service.findCommentsBoardId(boardId);
 		return new Response<>(HttpStatus.OK.value(),list);
 	}
-	
+
+	//댓글 작성
 	@PostMapping("/write/{id}")
 	public Response<?>replyWrite(@PathVariable(value="id")Integer boardId,
 								 @Valid @RequestBody CommentDto.CommentRequestDto dto,
@@ -52,7 +55,8 @@ public class CommentApiController {
 		
 		return new Response<>(HttpStatus.OK.value(),insertResult);
 	}
-	
+
+	//댓글 삭제
 	@DeleteMapping("/delete/{id}")
 	public Response<?>replyDelete(@PathVariable(value="id")Integer replyId){
 
@@ -61,6 +65,7 @@ public class CommentApiController {
 
 		return new Response<>(HttpStatus.OK.value(),"o.k");
 	}
+
 
 	private Member getMember(){
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
