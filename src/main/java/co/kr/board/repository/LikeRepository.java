d404747170d2a2220a95c6f66ed2feaceeea50bc
well0924 <well414965@gmail.com>
2023-10-29 17:57:10 +0900

    feat : 좋아요 갯수 및 인증 로직 변경

--- src/main/java/co/kr/board/repository/LikeRepository.java
+++ src/main/java/co/kr/board/repository/LikeRepository.java
@@ -4,10 +4,16 @@ import co.kr.board.domain.Board;
 import co.kr.board.domain.Like;
 import co.kr.board.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like,Long> {
     //좋아요 중복여부
     Optional<Like>findByMemberAndBoard(Member member, Board board);
+
+    //게시글 좋아요 갯수
+    @Query(value = "select count (*) from Like l where l.board.id = :id")
+    Integer findByBoardId(@Param("id") Integer boardId);
 }
--- src/main/java/co/kr/board/service/LikeService.java
+++ src/main/java/co/kr/board/service/LikeService.java
@@ -1,22 +1,25 @@
 package co.kr.board.service;
 
-import co.kr.board.config.redis.CacheKey;
 import co.kr.board.domain.Board;
-import co.kr.board.repository.BoardRepository;
 import co.kr.board.config.Exception.dto.ErrorCode;
 import co.kr.board.config.Exception.handler.CustomExceptionHandler;
 import co.kr.board.domain.Like;
 import co.kr.board.repository.LikeRepository;
 import co.kr.board.domain.Member;
+import co.kr.board.repository.MemberRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Log4j2
 @Service
 @AllArgsConstructor
 public class LikeService {
     private final LikeRepository repository;
+    private final MemberRepository memberRepository;
     private final String likeMessage ="좋아요 처리 완료";
     private final String likeCancelMessage ="좋아요 취소 처리 완료";
 
@@ -27,7 +30,9 @@ public class LikeService {
     * 게시글조회에서 좋아요 +1기능
     */
     @Transactional
-    public String createLikeBoard(Board board,Member member){
+    public String createLikeBoard(Board board){
+        Member member =getMember();
+        //좋아요 증가
         board.increaseLikeCount();
         Like like = new Like(board,member);
         repository.save(like);
@@ -41,8 +46,10 @@ public class LikeService {
      * 게시글 조회에서 좋아요 -1
      */
     @Transactional
-    public String removeLikeBoard(Board board,Member member){
+    public String removeLikeBoard(Board board){
+        Member member =getMember();
         Like likeBoard = repository.findByMemberAndBoard(member,board).orElseThrow(()->{throw new CustomExceptionHandler(ErrorCode.LIKE_NOT_FOUND);});
+        //좋아요 감소기능
         board.decreaseLikeCount();
         repository.delete(likeBoard);
         return likeCancelMessage;
@@ -55,7 +62,27 @@ public class LikeService {
     *
     */
     @Transactional
-    public boolean hasLikeBoard(Board board,Member member){
+    public boolean hasLikeBoard(Board board){
+        Member member =getMember();
         return repository.findByMemberAndBoard(member,board).isPresent();
+    }
+
+    /*
+     * 좋아요 갯수
+     */
+    @Transactional
+    public Integer LikeCount(Integer boardId){
+        return repository.findByBoardId(boardId);
+    }
+
+    /*
+     * 회원 인증
+     */
+    private Member getMember(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = (String)authentication.getName().toString();
+        log.info(username);
+        Member member = memberRepository.findByUsername(username).orElseThrow(()->new CustomExceptionHandler(ErrorCode.NOT_FOUND));
+        return member;
     }
 }
