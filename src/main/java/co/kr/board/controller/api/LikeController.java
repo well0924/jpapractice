cc210f607723e2a8d5a45465a4a8f479ed2e7b47
well0924 <well414965@gmail.com>
2023-10-29 17:56:37 +0900

    refactor : 좋아요 권한 추가

--- src/main/java/co/kr/board/controller/api/LikeController.java
+++ src/main/java/co/kr/board/controller/api/LikeController.java
@@ -7,55 +7,49 @@ import co.kr.board.config.redis.CacheKey;
 import co.kr.board.domain.Board;
 import co.kr.board.repository.BoardRepository;
 import co.kr.board.service.LikeService;
-import co.kr.board.domain.Member;
-import co.kr.board.repository.MemberRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+@Log4j2
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/like/*")
 public class LikeController {
     private final BoardRepository boardRepository;
     private final LikeService likeService;
-    private final MemberRepository memberRepository;
-    
+
     //좋아요+1기능
-    @Secured({"ROLE_ADMIN,ROLE_USER"})
+    @Secured({"ROLE_ADMIN","ROLE_USER"})
     @PostMapping("/plus/{id}")
+    @Cacheable(value = CacheKey.LIKE,key = "#boardId",unless = "#result == null")
     public Response<String> likeBoard(@PathVariable(value = "id")Integer boardId){
-        Member member = getMember();
         Board board = boardRepository.findById(boardId).orElseThrow(() -> new CustomExceptionHandler(ErrorCode.NOT_BOARD_DETAIL));
-        String likeResult =likeService.createLikeBoard(board, member);
+        String likeResult =likeService.createLikeBoard(board);
         return new Response<>(HttpStatus.OK.value(),likeResult);
     }
 
     //좋아요-1기능
-    @Secured({"ROLE_ADMIN,ROLE_USER"})
+    @Secured({"ROLE_ADMIN","ROLE_USER"})
+    @CacheEvict(value = CacheKey.LIKE,key = "#boardId")
     @PostMapping("/minus/{id}")
     public Response<String> minusBoard(@PathVariable(value = "id")Integer boardId){
-        Member member = getMember();
         Board board = boardRepository.findById(boardId).orElseThrow(()->new CustomExceptionHandler(ErrorCode.NOT_BOARD_DETAIL));
-        String likeResult = likeService.removeLikeBoard(board,member);
+        String likeResult = likeService.removeLikeBoard(board);
         return new Response<>(HttpStatus.OK.value(),likeResult);
     }
 
     //좋아요 중복기능
-    @Secured({"ROLE_ADMIN,ROLE_USER"})
+    @Secured({"ROLE_ADMIN","ROLE_USER"})
     @GetMapping("/duplicated/{id}")
-    @Caching(cacheable = {@Cacheable(value = CacheKey.LIKE,key = "#boardId",unless = "#result == null")},
-            evict = {@CacheEvict(value = CacheKey.LIKE,key = "#boardId")})
     public Response<Boolean> likeDuplicated(@PathVariable(value = "id") Integer boardId){
-        Member member = getMember();
         Board board = boardRepository.findById(boardId).orElseThrow(()->new CustomExceptionHandler(ErrorCode.NOT_BOARD_DETAIL));
-        boolean result = likeService.hasLikeBoard(board, member);
-
+        boolean result = likeService.hasLikeBoard(board);
+        log.info("result::"+result);
         if(result == true){
             minusBoard(boardId);
         }else{
@@ -64,10 +58,4 @@ public class LikeController {
         return new Response<>(HttpStatus.OK.value(),result);
     }
 
-    private Member getMember(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = (String)authentication.getPrincipal();
-        Member member = memberRepository.findByUsername(username).orElseThrow(()->new CustomExceptionHandler(ErrorCode.NOT_FOUND));
-        return member;
-    }
 }
