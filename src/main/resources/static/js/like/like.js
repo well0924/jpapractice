37763d381b9c5630925d3c9c83048c720b21b21b
well0924 <well414965@gmail.com>
2023-10-30 17:35:22 +0900

    refactor : 스크립트 파일 이동

---
+++ src/main/resources/static/js/like/like.js
@@ -0,0 +1,47 @@
+/*
+* 게시글 좋아요
+*/
+
+$(document).ready(function(){
+    //좋아요 갯수
+    likeCount();
+});
+//좋아요 갯수
+function likeCount(){
+    let id = $('#boardid').val();
+    let token = localStorage.getItem('Authorization');
+    let result = JSON.parse(token);
+    $.ajax({
+        url:'/api/like/count/'+id,
+        type:'get',
+        dataTye: 'json',
+        contentType: 'application/json; charset = utf-8',
+        headers:{'Authorization': 'Bearer '+result.value}
+    }).done(function(resp){
+        console.log(resp.data);
+
+        $('#likecount').html(resp.data);
+    });
+}
+//좋아요 중복기능
+function addDuplicatedLike(){
+    let id = $('#boardid').val();
+    let token = localStorage.getItem('Authorization');
+    let result = JSON.parse(token);
+    $.ajax({
+        url:'/api/like/duplicated/'+id,
+        type:'get',
+        headers:{'Authorization':  'Bearer '+result.value},
+        contentType:'application/json; charset = utf-8',
+        dataTye: 'json'
+    }).done(function(resp){
+        console.log(resp);
+        console.log(resp.data);
+        if(resp.data== true){
+            alert("좋아요 취소");
+        }
+        if(resp.data== false){
+            alert("좋아요 추가");
+        }
+    });
+}
