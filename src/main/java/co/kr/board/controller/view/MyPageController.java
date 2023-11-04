ec7c211184ff30d8a48c6f797411a467e696519d
well0924 <well414965@gmail.com>
2023-11-04 18:25:51 +0900

    feat : 회원 마이페이지

---
+++ src/main/java/co/kr/board/controller/view/MyPageController.java
@@ -0,0 +1,90 @@
+package co.kr.board.controller.view;
+
+import co.kr.board.domain.Dto.BoardDto;
+import co.kr.board.domain.Dto.CommentDto;
+import co.kr.board.domain.Dto.ScrapDto;
+import co.kr.board.domain.Member;
+import co.kr.board.service.BoardService;
+import co.kr.board.service.CommentService;
+import co.kr.board.service.ScrapService;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Log4j2
+@Controller
+@RequestMapping("/page/mypage")
+@AllArgsConstructor
+public class MyPageController {
+
+    private final ScrapService scrapService;
+    private final BoardService boardService;
+    private final CommentService commentService;
+
+    //마이페이지 스크랩 목록
+    @GetMapping("/list")
+    public ModelAndView myPageList(@PageableDefault(size = 10,direction = Sort.Direction.DESC,sort = "id") Pageable pageable){
+        ModelAndView mv = new ModelAndView();
+        //스크랩 한 게시글 목록
+        Page<ScrapDto.ResponseDto> scrapList = scrapService.scrapList(pageable);
+        //최근에 작성한 글(5개)
+        List<BoardDto.BoardResponseDto> top5 = boardService.findBoardTop5();
+        //회원 조회
+
+        mv.addObject("scrapList",scrapList);
+        mv.addObject("top5",top5);
+        mv.setViewName("/mypage/mypage");
+
+        return mv;
+    }
+
+    //회원이 작성한 글
+    @GetMapping("/my-artice/{username}")
+    public ModelAndView myBoard(@PathVariable("username") String username,@PageableDefault(sort = "id",direction = Sort.Direction.DESC,size = 5) Pageable pageable){
+        ModelAndView mv = new ModelAndView();
+        //회원이 작성한글
+        Page<BoardDto.BoardResponseDto>list = boardService.memberArticle(username,pageable);
+        //스크랩 한 게시글 목록
+        Page<ScrapDto.ResponseDto> scrapList = scrapService.scrapList(pageable);
+        //최근에 작성한 글(5개)
+        List<BoardDto.BoardResponseDto> top5 = boardService.findBoardTop5();
+
+        mv.addObject("list",list);
+        mv.addObject("scrapList",scrapList);
+        mv.addObject("top5",top5);
+
+        mv.setViewName("/mypage/myarticle");
+
+        return mv;
+    }
+
+    //회원이 작성한 댓글
+    @GetMapping("/my-comment/{username}")
+    public ModelAndView myComment(@PathVariable("username") String username,@PageableDefault(sort = "id",direction = Sort.Direction.DESC,size = 5) Pageable pageable) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        //회원이 작성한 댓글
+        Page<CommentDto.CommentResponseDto>list = commentService.getMyComment(username,pageable);
+        //스크랩 한 게시글 목록
+        Page<ScrapDto.ResponseDto> scrapList = scrapService.scrapList(pageable);
+        //최근에 작성한 글(5개)
+        List<BoardDto.BoardResponseDto> top5 = boardService.findBoardTop5();
+
+        mv.addObject("list",list);
+        mv.addObject("scrapList",scrapList);
+        mv.addObject("top5",top5);
+
+        mv.setViewName("/mypage/mycomment");
+
+        return mv;
+    }
+}
