64812c0a671fb01bfcd724cf94c5bf86f7bb87e3
well0924 <well414965@gmail.com>
2023-11-12 12:56:41 +0900

    refactor : 게시물 스크랩 기능

--- src/main/resources/static/js/scrap/scrap.js
+++ src/main/resources/static/js/scrap/scrap.js
@@ -1,10 +1,16 @@
 //게시물 스크랩 추가하기.
 function addScrap(){
-    let id = document.getElementById("boardid");
+    //로컬 스토리지에 저장된 at
+    let token = getItems();
+    let result = token.value;
+    let id = document.getElementById("boardid").value;
     console.log(id);
     $.ajax({
         url:'/api/scrap/duplicate/'+id,
         type:'get',
+        headers:{
+            'Authorization': 'Bearer '+result,
+        },
         dataType:'json'
     }).done(function(resp){
         console.log(resp.data);
