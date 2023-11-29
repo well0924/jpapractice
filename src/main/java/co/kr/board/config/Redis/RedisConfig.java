32069e43f54da22ded4ead66df23842e78892dc4
well0924 <well414965@gmail.com>
2023-11-29 21:59:10 +0900

    docs : redis설정 주석

--- src/main/java/co/kr/board/config/Redis/RedisConfig.java
+++ src/main/java/co/kr/board/config/Redis/RedisConfig.java
@@ -35,25 +35,25 @@ public class RedisConfig {
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(redisHost, redisPort);
     }
-
+    
+    //redisTemplate을 사용
     @Bean
     public RedisTemplate<?,?> redisTemplate() {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
-
+        
         redisTemplate.setConnectionFactory(redisConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-
+        redisTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
         return redisTemplate;
     }
-
+    
+    //redisCache를 사용
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory){
-
+        //redisCache설정
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
                 .defaultCacheConfig()
                 .disableCachingNullValues()//null value는 캐시를 안함.
@@ -64,7 +64,7 @@ public class RedisConfig {
 
         //캐시키별  default 유효기간 설정
         Map<String,RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
-
+        //캐시를 사용할 때 각각의 유효기간을 설정
         cacheConfigurations.put(CacheKey.USER, RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(CacheKey.USER_EXPIRE_SEC)));
         cacheConfigurations.put(CacheKey.BOARD,RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(CacheKey.BOARD_EXPIRE_SEC)));
         cacheConfigurations.put(CacheKey.LIKE,RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(CacheKey.LIKE_EXPIRE_SEC)));
