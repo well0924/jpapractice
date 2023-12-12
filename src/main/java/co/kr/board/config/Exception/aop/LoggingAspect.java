6144e430c9edf8bbf6e1db4dd3e135e659a3eac0
well0924 <well414965@gmail.com>
2023-12-13 03:37:14 +0900

    feat : 파라미터 로깅

---
+++ src/main/java/co/kr/board/config/Exception/aop/LoggingAspect.java
@@ -0,0 +1,70 @@
+package co.kr.board.config.Exception.aop;
+
+import com.google.common.base.Joiner;
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Log4j2
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Around("execution(* co.kr.board.*..*ApiController.*(..))")
+    public Object logging(ProceedingJoinPoint pjp) throws Throwable { // 2
+
+        String params = getRequestParams(); // request 값 가져오기
+
+        long startAt = System.currentTimeMillis();
+
+        log.info("-----------> REQUEST : {}({}) = {}", pjp.getSignature().getDeclaringTypeName(),
+                pjp.getSignature().getName(), params);
+
+        Object result = pjp.proceed(); // 4
+
+        long endAt = System.currentTimeMillis();
+
+        log.info("-----------> RESPONSE : {}({}) = {} ({}ms)", pjp.getSignature().getDeclaringTypeName(),
+                pjp.getSignature().getName(), result, endAt - startAt);
+
+        return result;
+    }
+
+    private String paramMapToString(Map<String, String[]> paramMap) {
+        return paramMap.entrySet().stream()
+                .map(entry -> String.format("%s -> (%s)",
+                        entry.getKey(), Joiner.on(",").join(entry.getValue())))
+                .collect(Collectors.joining(", "));
+    }
+
+    // Get request values
+    private String getRequestParams() {
+
+        String params = "없음";
+
+        RequestAttributes requestAttributes = RequestContextHolder
+                .getRequestAttributes(); // 3
+
+        if (requestAttributes != null) {
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+                    .getRequestAttributes()).getRequest();
+
+            Map<String, String[]> paramMap = request.getParameterMap();
+            if (!paramMap.isEmpty()) {
+                params = " [" + paramMapToString(paramMap) + "]";
+            }
+        }
+
+        return params;
+
+    }
+}
