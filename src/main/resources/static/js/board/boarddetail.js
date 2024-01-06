63bc34a7612f7bb9a1241a031302121fe65c6fd8
well0924 <well414965@gmail.com>
2024-01-07 00:38:39 +0900

    refactor : 비밀글 이동

--- src/main/resources/static/js/board/boarddetail.js
+++ src/main/resources/static/js/board/boarddetail.js
@@ -8,13 +8,6 @@ function listpage(){
 }
 //수정페이지 o.k
 function modifypage(){
-	
 	let id = $('#boardid').val();
-	let pwd = $('#board-password').val();
-	//비밀번호가 있으면 비밀번호 확인 부분으로 없는 경우에는 게시글 수정화면으로 이동하기.
-	if(pwd != null){
-		location.href='/page/board/passwordcheck';
-	}else{
-		location.href='/page/board/modify/'+id;
-	}
+	location.href='/page/board/modify/'+id;
 }
